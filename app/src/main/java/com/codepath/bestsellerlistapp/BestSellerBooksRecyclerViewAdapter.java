5bf077acf6823254ddc0eb5bff32a1ab374a75e6
Ali <1130aliz@gmail.com>
2021-01-28 21:50:31 -0500

    add books layout

--- app/src/main/java/com/codepath/bestsellerlistapp/BestSellerBooksRecyclerViewAdapter.java
+++ app/src/main/java/com/codepath/bestsellerlistapp/BestSellerBooksRecyclerViewAdapter.java
@@ -3,13 +3,16 @@ package com.codepath.bestsellerlistapp;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
-import androidx.recyclerview.widget.RecyclerView;
-
+import com.bumptech.glide.Glide;
 import com.codepath.bestsellerlistapp.models.BestSellerBook;
 
 import java.util.List;
+
+import androidx.recyclerview.widget.RecyclerView;
 
 /**
  * {@link RecyclerView.Adapter} that can display a {@link BestSellerBook} and makes a call to the
@@ -38,6 +41,16 @@ public class BestSellerBooksRecyclerViewAdapter extends RecyclerView.Adapter<Bes
         holder.mBookTitle.setText(books.get(position).title);
         holder.mBookAuthor.setText(books.get(position).author);
 
+        BestSellerBook bestSellerBook = books.get(position);
+        Glide.with(holder.mView)
+                .load(bestSellerBook.bookImageUrl)
+                .centerInside()
+                .into(holder.mBookImage);
+
+        String ranking = String.format("%d", bestSellerBook.rank);
+        holder.mBookRanking.setText(ranking);
+        holder.mBookDescription.setText(books.get(position).description);
+
         holder.mView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -61,11 +74,22 @@ public class BestSellerBooksRecyclerViewAdapter extends RecyclerView.Adapter<Bes
         public final TextView mBookAuthor;
         public BestSellerBook mItem;
 
+        public final TextView mBookRanking;
+        public final ImageView mBookImage;
+        public final TextView mBookDescription;
+        public final Button mBuyBtn;
+
+
         public BookViewHolder(View view) {
             super(view);
             mView = view;
             mBookTitle = (TextView) view.findViewById(R.id.book_title);
             mBookAuthor = (TextView) view.findViewById(R.id.book_author);
+
+            mBookRanking= (TextView) view.findViewById(R.id.ranking);
+            mBookImage= view.findViewById(R.id.book_image);
+            mBookDescription= (TextView) view.findViewById(R.id.book_description);
+            mBuyBtn= view.findViewById(R.id.buy_button);
         }
 
         @Override
